7b0913390db4cdfc9b662c080090319afffabe08
EitanBoskovitz <eiboeibo1@gmail.com>
2023-06-25 15:47:33 +0300

    added login backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {StreamChat} = require('stream-chat');
+const { StreamChat } = require('stream-chat');
 const { v4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const app = express();
@@ -18,15 +18,33 @@ app.post("/signup", async (req, res) => {
         const userID = v4();
         const hashedPassword = await bcrypt.hash(password, 10);
         const token = serverClient.createToken(userID);
-        res.json({token, userID, userName, hashedPassword });
-    } catch(error) {
+        res.json({ token, userID, userName, hashedPassword });
+    } catch (error) {
+        res.json(error);
+    }
+
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        const { userName, password } = req.body
+        const { users } = await serverClient.queryUsers({ name: userName });
+        if (users.length === 0) return res.json({ message: "user not found" });
+
+        const token = serverClient.createToken(users[0].id);
+        const passwordMatch = await bcrypt.compare(password, users[0].hashedPassword)
+
+        if (passwordMatch) {
+            res.json({
+                token,
+                userName,
+                userID: users[0].id,
+            })
+        }
+    }catch(error){
         res.json(error);
     }
     
-})
-
-app.post("/login", (req, res) => {
-    const {userName, password} = req.body
 })
 
 app.listen(3001, () => {
