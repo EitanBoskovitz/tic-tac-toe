f9d527ff66d8db8c4315b3eecf94afd2b16ea08a
EitanBoskovitz <eiboeibo1@gmail.com>
2023-06-25 14:14:34 +0300

    added Login component based on the signup component

---
+++ client/src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function Login() {
+    const [user, setUser] = useState(null);
+    const login = () => {}
+    return (
+        <div>
+            <label>Login</label>
+            <input placeholder="User Name" onChange={(event) => {
+                setUser({ ...user, username: event.target.value })
+            }} />
+            <input placeholder="Password" onChange={(event) => {
+                setUser({ ...user, password: event.target.value })
+            }} />
+            <button onClick={login}/> Login
+        </div>
+    )
+}
